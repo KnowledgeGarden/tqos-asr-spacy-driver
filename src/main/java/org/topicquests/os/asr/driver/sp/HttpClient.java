8137a1b8cba8f13cb3a7c303196f41e4a247ce0f
Jack Park <jackpark@gmail.com>
2021-01-30 17:34:53 -0800

    updated to deal with bad respose code

--- src/main/java/org/topicquests/os/asr/driver/sp/HttpClient.java
+++ src/main/java/org/topicquests/os/asr/driver/sp/HttpClient.java
@@ -26,37 +26,51 @@ public class HttpClient {
 	 * Process a sentence {@code queryString}, a JSON string
 	 * @param url
 	 * @param queryString
-	 * @return
+	 * @return can return {@code null}
 	 */
 	public IResult put(String url, String queryString) {
 		IResult result = new ResultPojo();
 		BufferedReader rd = null;
 		HttpURLConnection con = null;
+		OutputStream os = null;
+		PrintWriter p = null;
+		int counter = 0;
 		try {
 			URL urx = new URL(url);
-
-			con = (HttpURLConnection) urx.openConnection();
-			//con.setReadTimeout(TIMEOUT);
-			con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-			con.setRequestMethod("POST");
-			con.setDoInput(true);
-			con.setDoOutput(true);
-			OutputStream os = con.getOutputStream();
-			PrintWriter p = new PrintWriter(os);
-			p.print(queryString);
-			p.flush();
-			p.close();
-			//con.connect();
-			System.out.println("CODE "+con.getResponseCode());
-			rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			StringBuilder buf = new StringBuilder();
-
-			String line;
-			while ((line = rd.readLine()) != null) {
-				buf.append(line + '\n');
+			while (counter < 4) {
+				try {
+					con = (HttpURLConnection) urx.openConnection();
+					//con.setReadTimeout(TIMEOUT);
+					con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+					con.setRequestMethod("POST");
+					con.setDoInput(true);
+					con.setDoOutput(true);
+					os = con.getOutputStream();
+					p = new PrintWriter(os);
+					p.print(queryString);
+					p.flush();
+					p.close();
+				} catch (Exception x) {
+					environment.logError(x.getMessage()+" Count="+counter, x);
+					try {
+						wait(3000); // short delay
+					} catch (Exception xyz) {};
+					counter++;
+				}
+			}
+			int code = con.getResponseCode();
+			System.out.println("CODE "+code);
+			if (code == 200) {
+				rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				StringBuilder buf = new StringBuilder();
+	
+				String line;
+				while ((line = rd.readLine()) != null) {
+					buf.append(line + '\n');
+				}
+				result.setResultObject(buf.toString());
 			}
 
-			result.setResultObject(buf.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 			result.addErrorString(e.getMessage());
